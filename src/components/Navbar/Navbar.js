3309684e593b5cf7db0379aa918bf9a82c27b88f
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-10 11:59:30 +0300

    link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Logo from '../../assets/logo.png'
+import Link from 'react-scroll'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <nav className='navbar'>
+        <img src={Logo} className='logo' />
+        <div className='desktopMenu'>
+          <Link classNamedesktopMenuListItem></Link>
+        </div>
+        <button className='' type='submit'>
+          <img src='' className='desktopMenuImg' /> Contact Me
+        </button>
+      </nav>
+    </div>
   )
 }
 
